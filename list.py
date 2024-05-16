1ad30336474d76ac524a80679a8e29645d1bc3eb
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2024-05-17 01:32:21 +0600

    remove and append command added

--- list.py
+++ list.py
@@ -18,3 +18,9 @@ command = input().split()
 for i in range(N):
     if command[0] == "insert":
         mylist.insert(int(command[1], int(command[2])))
+    elif command[0] == "print":
+        print(mylist)
+    elif command[0] == "remove":
+        mylist.remove(int(command[1]))
+    elif command[0] == "append":
+        mylist.append(int(command[1]))
