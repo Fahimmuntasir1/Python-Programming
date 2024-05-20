52dde50cf543c64c99b2e470d26307b1dca0affa
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2024-05-20 13:52:19 +0600

    text wrapper problem has solved

--- txtWrap.py
+++ txtWrap.py
@@ -32,3 +32,17 @@
 # QRST
 # UVWX
 # YZ
+
+
+import textwrap
+
+
+def wrap(string, max_width):
+    r = textwrap.fill(string, max_width)
+    return r
+
+
+if __name__ == "__main__":
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
